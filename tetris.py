02503dc7758c3239c75621c27d65bc9954e8efc3
Bryce Farnsworth <bryce.farnsworth@gmail.com>
2015-09-08 18:55:27 -0700

    Made blocks fall. They don't stack on top of each other yet.

--- tetris.py
+++ tetris.py
@@ -1,21 +1,43 @@
 import pygame
+import random
 
 BLACK = (0, 0, 0)
 GRAY = (140, 140, 140)
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
 BLOCK_WIDTH = 25
 BLOCK_HEIGHT = 25
 MARGIN = 5
+
+GRID_X = 270
+GRID_Y = 45
+GRID_WIDTH = 260
+GRID_HEIGHT = 510
+
+BLOCK_SPRITES = ["block_red.png", "block_orange.png", "block_yellow.png",
+                 "block_green.png", "block_cyan.png", "block_blue.png",
+                 "block_purple.png"]
 
 class Block(pygame.sprite.Sprite):
     """ Represents a single block of a tetromino. """
     def __init__(self, x, y, img):
         super().__init__()
 
+        self.falling = True
+
         self.image = pygame.image.load(img).convert()
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
+    def update(self):
+        if self.falling:
+            self.rect.y += BLOCK_HEIGHT
+            if self.rect.y > GRID_Y + GRID_HEIGHT - MARGIN - BLOCK_HEIGHT:
+                self.rect.y = GRID_Y + GRID_HEIGHT - MARGIN - BLOCK_HEIGHT
+                self.falling = False
 
 
 class Tetromino:
@@ -26,8 +48,6 @@ class Tetromino:
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 pygame.display.set_caption("Tetris")
@@ -35,30 +55,14 @@ pygame.display.set_caption("Tetris")
 # Grid consists of 20 rows, but we'll keep 4 extra for blocks to go
 # offscreen when the player loses
 grid = [[0] * 10] * 24
-GRID_X = 270
-GRID_Y = 45
-GRID_HEIGHT = 510
-GRID_WIDTH = 260
 
 block_list = pygame.sprite.Group()
 
-block = Block(275, 525, "block_red.png")
-block_list.add(block)
-block = Block(300, 525, "block_orange.png")
-block_list.add(block)
-block = Block(325, 525, "block_yellow.png")
-block_list.add(block)
-block = Block(350, 525, "block_green.png")
-block_list.add(block)
-block = Block(375, 525, "block_cyan.png")
-block_list.add(block)
-block = Block(400, 525, "block_blue.png")
-block_list.add(block)
-block = Block(425, 525, "block_purple.png")
-block_list.add(block)
-
 clock = pygame.time.Clock()
 done = False
+
+falling_block = Block(375, 50, BLOCK_SPRITES[random.randrange(7)])
+block_list.add(falling_block)
 
 while not done:
 
@@ -66,6 +70,18 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and falling_block.rect.x > GRID_X + MARGIN:
+        falling_block.rect.x -= BLOCK_WIDTH
+    elif keys[pygame.K_RIGHT] and falling_block.rect.x < GRID_X + GRID_WIDTH - MARGIN - BLOCK_WIDTH:
+        falling_block.rect.x += BLOCK_WIDTH
+
+    if not falling_block.falling:
+        falling_block = Block(375, 50, BLOCK_SPRITES[random.randrange(7)])
+        block_list.add(falling_block)
+        
+    block_list.update()
+    
 
     # Draw the screen
     screen.fill(GRAY)
@@ -77,6 +93,6 @@ while not done:
 
     pygame.display.flip()
 
-    clock.tick(60)
+    clock.tick(30)
 
 pygame.quit()
